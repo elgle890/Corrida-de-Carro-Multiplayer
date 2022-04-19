172f73138685df238393a42c6a9670abd7e90862
“elgle890” <arthurpecegueiro2@gmail.com>
2022-04-18 21:11:11 -0300

    criação do placar e botão reset Co-authored-by: Psilva009 <Psilva009@users.noreply.github.com>

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,12 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.resetButton = createButton('');
+    this.resetTitle = createElement('h2');
+
+    this.leaderTitle = createElement('h2');
+    this.leader1 = createElement('h2');
+    this.leader2 = createElement('h2');
+  }
 
   start() {
     form = new Form();
@@ -35,18 +42,37 @@ class Game {
 
   handleElements() {
     form.hide();
-    form.titleImg.position(40, 50);
+    form.titleImg.position(width/2 - 150, 50);
     form.titleImg.class("gameTitleAfterEffect");
+    this.resetTitle.position(width/2 + 350, 40);
+    this.resetTitle.html('reset game');
+    this.resetTitle.class('resetTitle');
+
+    this.resetButton.position(width/2 + 380, 100);
+    this.resetButton.class('resetButton');
+
+    this.leaderTitle.position(width/18, 40);
+    this.leaderTitle.html('placar');
+    this.leaderTitle.class('resetTitle');
+
+    this.leader1.position(width/18, 80);
+    this.leader1.html('leader1');
+    this.leader1.class('leadersText');
+    this.leader2.position(width/18, 130);
+    this.leader2.html('leader2');
+    this.leader2.class('leadersText');
   }
 
   play() {
     this.handleElements();
+    this.handleMousePressedResetButton();
     Player.getInfosPlayer();
     
     
     
     if(players != undefined) {
       image(trackImg, 0, -height * 5, width, height * 6);
+      this.showLeaderboard();
       var index = 0;
       for(var plr in players) {
         var x = players[plr].positionX;
@@ -61,13 +87,10 @@ class Game {
           fill('red');
           ellipse(x, y, 60, 60);
           camera.position.x = width/2;
-          camera.position.y = y;
-
-          console.log(y);
+          if(players[plr].positionY > height) {
+            camera.position.y = y;
+          }
         }
-
-        
-        
       }
 
       this.handlePlayerController();
@@ -81,5 +104,69 @@ class Game {
       player.positionY += 10;
       player.update();
     }
+
+    if(keyIsDown(39)) {
+      player.positionX += 10;
+      player.update();
+    }
+
+    if(keyIsDown(37)) {
+      player.positionX -= 10;
+      player.update();
+    }
+  }
+
+  handleMousePressedResetButton() {
+    this.resetButton.mousePressed(() => {
+      database.ref('/').set({
+        playerCount: 0,
+        gameState: 0,
+        players: {}
+      });
+      window.location.reload();
+    });
+  }
+
+  showLeaderboard() {
+    var leader1, leader2;
+    var player = Object.values(players);
+    if (
+      (player[0].rank === 0 && player[1].rank === 0) ||
+      player[0].rank === 1
+    ) {
+      // &emsp;    Essa etiqueta é usada para exibir quatro espaços.
+      leader1 =
+        player[0].rank +
+        "&emsp;" +
+        player[0].name +
+        "&emsp;" +
+        player[0].score;
+
+      leader2 =
+        player[1].rank +
+        "&emsp;" +
+        player[1].name +
+        "&emsp;" +
+        player[1].score;
+    }
+
+    if (player[1].rank === 1) {
+      leader1 =
+        player[1].rank +
+        "&emsp;" +
+        player[1].name +
+        "&emsp;" +
+        player[1].score;
+
+      leader2 =
+        player[0].rank +
+        "&emsp;" +
+        player[0].name +
+        "&emsp;" +
+        player[0].score;
+    }
+
+    this.leader1.html(leader1);
+    this.leader2.html(leader2);
   }
 }
